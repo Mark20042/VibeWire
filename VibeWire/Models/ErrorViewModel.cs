c08552669554198332ba3b87aa8959a1cb9dba93
Mark20042 <mark.potot2004@gmail.com>
2026-06-16 10:31:34 +0800

    Add project files.

---
+++ VibeWire/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VibeWire.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
