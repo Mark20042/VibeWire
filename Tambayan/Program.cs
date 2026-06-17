ab60d9a05f96b5bec9ab13c67e1c807bf7ed4fba
Mark20042 <mark.potot2004@gmail.com>
2026-06-17 14:51:50 +0800

    initialize sql server database using entity framework database

--- Tambayan/Program.cs
+++ Tambayan/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Tambayan.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Database Config
+string dbConnectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(dbConnectionString));
 
 var app = builder.Build();
 
